279ab5f8804215d58691aca88bd0432083b0708e
Paul Hawke <paul.hawke@gmail.com>
2023-10-16 09:18:21 -0500

    Lower min API to test on more devices

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.sidharth.mosam"
-        minSdk = 30
+        minSdk = 28
         targetSdk = 34
         versionCode = 1
         versionName = "1.0.0"
